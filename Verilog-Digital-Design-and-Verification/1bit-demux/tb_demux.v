31e1d79ec966f05e73336f8d1e050a0190550257
Caleb Joseph <124648231+CalebJ28@users.noreply.github.com>
2025-08-03 23:19:49 -0400

    Create tb_demux.v

---
+++ Verilog-Digital-Design-and-Verification/1bit-demux/tb_demux.v
@@ -0,0 +1,41 @@
+`timescale 1us/1ns
+
+module tb_demux();
+
+	//declare nets and variables
+	reg x;
+	reg sel;
+	wire y0, y1;
+	
+	//instantiate the DUT
+	demux_1bit DEMUX1(
+		.x(x), 
+		.sel(sel), 
+		.y0(y0),
+		.y1(y1)
+	
+	
+	);
+	
+	//toggle x(data source)
+	initial begin
+		#1;  x = 1; 
+		#1;  x = 0; 
+		#2;  x = 1; 
+		#1.5;  x = 0; 
+		#0.5;  x = 1; 
+		#2; 
+	
+	end
+	
+	//toggle sel (select line)
+	initial begin
+		#1;  sel = 0; 
+		#1;  sel = 1; 
+		#2;  sel = 1; 
+		#1.8;  sel = 0; 
+		#1;  sel = 1; 
+		#1;  sel = 0; 
+		#5; 
+	end
+endmodule
